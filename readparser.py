e64246b07a787e872e1d72812bf3e70f768a8867
Adam Rivers <rivers@alum.mit.edu>
2016-09-09 16:06:18 -0700

    added a script to pasre out reads, bases and uniuqe kmers from each alignment report

---
+++ readparser.py
@@ -0,0 +1,33 @@
+#/env/bin/python
+# Scripts to test design the optimal PNA sequence from subsequence
+import argparse
+import os
+import re
+
+parser = argparse.ArgumentParser(description='parse out coverage from btools logs')
+parser.add_argument('--input', help="the input direcory containing log files", default = "logs_k31count_20160721")
+parser.add_argument('--out', help="the output report file", type=argparse.FileType('w'), default = '-')
+args = parser.parse_args()
+    
+abspath=os.path.abspath(args.input)
+args.out.write("name" + "\t" + "reads" + "\t" + "bases" + "\t" + "unique kmers" + "\n")
+for file in os.listdir(abspath):
+	with open(os.path.join(abspath,file), 'r') as file:
+		for line in file:
+			nameline = re.search("khist",line)
+			if nameline:
+				name = nameline.string.split()[3][3:-1]
+				continue
+			else:
+				inputline = re.search("Input", line)
+				if inputline:
+					reads = inputline.string.split()[1]
+					bases = inputline.string.split()[3]
+					continue
+				else:
+					uniqueline = re.search("Unique Kmers:",line)
+					if uniqueline:
+						unique = uniqueline.string.split()[2]
+						break
+		args.out.write(name + "\t" + reads + "\t" + bases + "\t" + unique + "\n")
+		
